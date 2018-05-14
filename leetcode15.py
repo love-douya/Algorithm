d0d677ead4540c5d3158ddc6761ecfd2c00df03b
love-douya <936165716@qq.com>
2018-05-15 07:32:46 +0800

    leetcode15

--- leetcode15.py
+++ leetcode15.py
@@ -1,3 +1,5 @@
+import sys
+
 class Solution1:
     def threeSum(self, nums):
         if len(nums) < 3:
@@ -8,9 +10,22 @@ class Solution1:
                 d[n] = 1
             else:
                 d[n] += 1
-        
-        for i, value in enumerate(nums):
-            
-
+        s = list(set(nums))
+        s.sort()
+        res = set()
+        for i, a in enumerate(s):
+            d[a] -= 1
+            for b in s[i:]:
+                if d[b] == 0:
+                    continue
+                c = - a - b
+                if c >= b and c in d and d[c] > (1 if c == b else 0):
+                    res.add((a, b, c))
+            d[a] = d[a] + 1
+        return list(res)
+ 
 if __name__ == "__main__":
-    print(Solution1().threeSum(input("Please input nums: ")))
+    nums = []
+    while sys.stdin.read() != '\n':
+        nums.append(int(sys.stdin.read()))
+    print(Solution1().threeSum(nums))
