e16b6044b4be7da2725569468c13cab66ed62343
love-douya <936165716@qq.com>
2018-05-17 22:18:35 +0800

    longest substring

--- leetcode3.py
+++ leetcode3.py
@@ -1,6 +1,17 @@
 class Solution:
     def lengthOfLongestSubstring(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
+        substr = ""
+        max_ = 0
+        d = {}
+        for i in range(len(s)):
+            if s[i] not in substr:
+               substr += s[i]
+            else:
+                substr = s[d[s[i]] + 1 : i + 1]
+            max_ = max(max_, len(substr))
+            d[s[i]] = i
+            #d.update({s[i] : i})
+        return max_
+
+if __name__ == "__main__":
+    print(Solution().lengthOfLongestSubstring("abdfakiad"))
