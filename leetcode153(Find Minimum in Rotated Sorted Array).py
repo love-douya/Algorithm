deeba13df7caef25f479474d1a7f9179e17a8103
love-douya <936165716@qq.com>
2018-06-17 19:58:14 +0800

    Update

--- leetcode153(Find Minimum in Rotated Sorted Array).py
+++ leetcode153(Find Minimum in Rotated Sorted Array).py
@@ -4,11 +4,10 @@ class Solution:
         :type nums: List[int]
         :rtype: int
         """
-        for i in len(nums) - 1:
-            if nums[i + 1] != nums[i] + 1:
+        for i in range(0, len(nums) - 1):
+            if nums[i + 1] < nums[i] + 1:
                 return nums[i + 1]
-            else:
-                return False 
+        return nums[0]
 
 if __name__ == "__main__":
     print(Solution().findMin(list(map(int, input("Input list: ")))))
