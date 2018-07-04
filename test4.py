8eeec3c974398c5c21edb0430363df7710ca95e6
Jimmy Tao <jimmyt@wicresoft.com>
2018-07-04 10:32:20 +0800

    Update

---
+++ test4.py
@@ -0,0 +1,96 @@
+import time
+
+class SuperClass:
+    superpower_list = []
+    superpower_var = 1
+
+    def __init__(self, name):
+        self.name = name
+
+    def add_superpower_list(self, power):
+        self.superpower_list.append(power)
+
+    def change_superpower_var(self, power):
+        self.superpower_var = power
+
+    @staticmethod
+    def superpower_static_method(var1, var2):
+        return var1 + var2
+
+    @classmethod
+    def superpower_class_method(cls, var):
+        cls.superpower_var = var
+
+#Initialize
+print("Initialize...")
+time.sleep(2)
+foo = SuperClass('foo')
+print("foo.name:")
+print(foo.name)
+
+#Test if list can be changed by instance
+print("foo.add_superpower_list('fly')")
+foo.add_superpower_list('fly')
+print(foo.superpower_list)
+print(SuperClass.superpower_list)
+
+print("foo.superpower_list.append('fly1')")
+foo.superpower_list.append('fly1')
+print(foo.superpower_list)
+print(SuperClass.superpower_list)
+
+print("SuperClass.superpower_list.append('fly2')")
+SuperClass.superpower_list.append('fly2')
+print(foo.superpower_list)
+print(SuperClass.superpower_list)
+
+#Test if variable can be changed by instance
+print("foo.change_superpower_var(1)")
+foo.change_superpower_var(1)
+print(foo.superpower_var)
+print(SuperClass.superpower_var)
+
+print("foo.superpower_var = 2")
+foo.superpower_var = 2
+print(foo.superpower_var)
+print(SuperClass.superpower_var)
+
+print("SuperClass.superpower_var = 3")
+SuperClass.superpower_var = 3
+print(foo.superpower_var)
+print(SuperClass.superpower_var)
+
+#Test if variable can be changed by cls method
+print("foo.superpower_class_method(4)")
+foo.superpower_class_method(4)
+print(foo.superpower_var)
+print(SuperClass.superpower_var)
+
+print("SuperClass.superpower_class_method(5)")
+SuperClass.superpower_class_method(5)
+print(foo.superpower_var)
+print(SuperClass.superpower_var)
+
+#Compare id of foo.mutable and SuperClass.mutable, foo.immutable and SuperClass.immutable
+print("id(SuperClass.superpower_list)")
+print(id(SuperClass.superpower_list))
+print(hex(id(SuperClass.superpower_list)))
+print("id(foo.superpower_list)")
+print(id(foo.superpower_list))
+print(hex(id(foo.superpower_list)))
+
+print("id(SuperClass.superpower_var)")
+print(id(SuperClass.superpower_var))
+print(hex(id(SuperClass.superpower_var)))
+print("id(foo.superpower_var)")
+print(id(foo.superpower_var))
+print(hex(id(foo.superpower_var)))
+
+#Test static method
+print("foo.superpower_static_method(1, 2)")
+print(foo.superpower_static_method(1, 2))
+print("SuperClass.superpower_static_method(2, 3)")
+print(SuperClass.superpower_static_method(2, 3))
+
+SuperClass.a = 3
+print(SuperClass.a)
