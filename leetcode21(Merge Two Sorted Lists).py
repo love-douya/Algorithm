cc192005238644135310719fb4c565673b4171be
Jimmy Tao <jimmyt@wicresoft.com>
2018-07-06 09:55:27 +0800

    Update

--- leetcode21(Merge Two Sorted Lists).py
+++ leetcode21(Merge Two Sorted Lists).py
@@ -8,6 +8,30 @@ class ListNode:
         self.val = x
         self.next = None
 
+# class Solution:
+#     def mergeTwoLists(self, l1, l2):
+#         """
+#         :type l1: ListNode
+#         :type l2: ListNode
+#         :rtype: ListNode
+#         """
+#         if not l1 : return l2
+#         if not l2 : return l1
+        
+#         curr = head = ListNode(0)        
+#         while l1 and l2:
+#             if l1.val < l2.val:                
+#                 curr.next = l1
+#                 l1=l1.next                
+#             else:                
+#                 curr.next = l2
+#                 l2=l2.next
+#             curr = curr.next
+        
+#         curr.next = l1 if l1 else l2
+            
+#         return head.next
+
 class Solution:
     def mergeTwoLists(self, l1, l2):
         """
@@ -15,35 +39,50 @@ class Solution:
         :type l2: ListNode
         :rtype: ListNode
         """
+
+        if not l1:
+            return l2
+        
+        if not l2:
+            return l1
+
         if l1.val <= l2.val:
-            root = copy.deepcopy(l1)
+            root = l1
             l1 = l1.next
         else:
-            root = copy.deepcopy(l2)
+            root = l2
             l2 = l2.next
 
-        l3 = copy.deepcopy(root)
+        l3 = root
 
         while l1 and l2:
             if l1.val <= l2.val:
-                l3.next = copy.deepcopy(l1)
+                l3.next = l1
                 l1 = l1.next
                 l3 = l3.next
             else:
-                l3.next = copy.deepcopy(l2)
+                l3.next = l2
                 l2 = l2.next
                 l3 = l3.next
         
+        #Runtime beats 97.38 %
         if l1:
-            while l2:
-                l3.next = copy.deepcopy(l2)
-                l2 = l2.next
-                l3 = l3.next
+            l3.next = l1
         else:
-            while l1:
-                l3.next = copy.deepcopy(l1)
-                l1 = l1.next
-                l3 = l3.next
+            l3.next = l2
+
+        # # Also Runtime Beats 97.38 %
+        # if l1:
+        #     while l1:
+        #         l3.next = l1
+        #         l1 = l1.next
+        #         l3 = l3.next
+
+        # else:
+        #     while l2:
+        #         l3.next = l2
+        #         l2 = l2.next
+        #         l3 = l3.next
 
         result = []
         while root:
@@ -54,18 +93,17 @@ class Solution:
 
 def Value2Instance(ValueList):
     Node = ListNode(ValueList[0])
-    Root = copy.deepcopy(Node)
+    Root = Node
     for i in range(1, len(ValueList)):
         Node.next = ListNode(ValueList[i])
         Node = Node.next
-    Node.next = None
 
-    #Test
-    result = []
-    while Root:
-        result.append(Root.val)
-        Root = Root.next
-    print(result)
+    # #Test
+    # result = []
+    # while Root:
+    #     result.append(Root.val)
+    #     Root = Root.next
+    # print(result)
 
     return Root
 
@@ -74,4 +112,5 @@ if __name__ == "__main__":
     l1 = list(map(int, list(str(sys.stdin.readline()).strip())))
     sys.stdout.write("Input list2: \n")    
     l2 = list(map(int, list(str(sys.stdin.readline()).strip())))
+    #print("l1: {0}\nl2: {1}".format(l1, l2))
     sys.stdout.write("Result is: \n" + str(Solution().mergeTwoLists(Value2Instance(l1), Value2Instance(l2))))
