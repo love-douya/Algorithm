9274de4dbdaaa142ff1b0fb14af934b6f3c1016c
Jimmy Tao <jimmyt@wicresoft.com>
2018-07-06 17:52:17 +0800

    Update

---
+++ leetcode22(Generate Parentheses).py
@@ -0,0 +1,12 @@
+import sys
+
+class Solution:
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        pass
+
+if __name__ == '__main__':
+    print('Result is: {0}'.format(Solution().generateParenthesis(int(input('Input number of parentheses pairs: ')))))
