ef9ca537c249ff603611a4b0702fabaf08ce51b1
Jimmy Tao <jimmyt@wicresoft.com>
2018-07-10 10:25:10 +0800

    Update

--- leetcode17(Letter Combinations of a Phone Number).py
+++ leetcode17(Letter Combinations of a Phone Number).py
@@ -4,19 +4,36 @@ class Solution:
         :type digits: str
         :rtype: List[str]
         """
-        dic = {'2': ['a', 'b', 'c'], '3': ['d', 'e', 'f'],
+        dic = {'0': [' '], '1': [''], '2': ['a', 'b', 'c'], '3': ['d', 'e', 'f'],
                '4': ['g', 'h', 'i'], '5': ['j', 'k', 'l'],
                '6': ['m', 'n', 'o'], '7': ['p', 'q', 'r', 's'],
                '8': ['t', 'u', 'v'], '9': ['w', 'x', 'y', 'z']}
-        phone_number = list(digits).remove('0').remove('1')
-        result = []
-        string = None
-        for i in phone_number:
-            for j in dic[i]:
-                for k in phone_number:
-                    result.append(string + j)
+    
+        result = ['']
+        if not digits:
+            return []
+        else:
+            # for i in digits:
+            #     for j in dic[i]:
+            #         for k in result:
+            #             [k + j]
+            for i in digits:
+                result = [k + j for j in dic[i] for k in result]
+            return result
 
-        return result
+class Solution1:
+    def letterCombinations(self, digits):
+        dict_num = { '1': '', '2': 'abc', '3': 'def',
+        '4': 'ghi', '5': 'jkl', '6': 'mno',
+        '7': 'pqrs','8': 'tuv', '9': 'wxyz',
+        '0': ' '}
+        if not digits:
+            return []
+        else:
+            answer = ['']
+            for i in digits:
+                answer = [b+a for a in dict_num[i] for b in answer] 
+            return answer
 
 if __name__ == '__main__':
     print(Solution().letterCombinations(input('Input the phone number: ')))
--- test5(about re).py
+++ test5(about re).py
@@ -5,3 +5,7 @@ print(m)
 
 m = re.findall(r'\bblow', '\bblow')
 print(m)
+
+k = []
+m = len(k)
+print(m)
