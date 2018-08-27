89a213b5850a8b40c0cab35564268629478afbf1
love-douya <936165716@qq.com>
2018-08-27 23:13:27 +0800

    Update

--- leetcode25(Reverse Nodes in k-Group).py
+++ leetcode25(Reverse Nodes in k-Group).py
@@ -1,3 +1,5 @@
+import sys
+
 # Definition for singly-linked list.
 class ListNode:
     def __init__(self, x):
@@ -18,10 +20,13 @@ def Value2Instance(ValueList):
     Root = Node
     for i in range(1, len(ValueList)):
         Node.next = ListNode(ValueList[i])
-        Node = Node.next
+        Node = Node.next 
     return Root
 
 if __name__ == "__main__":
     sys.stdout.write("Input list: \n")
-    l1 = list(map(int, list(str(sys.stdin.readline()).strip())))
-    sys.stdout.write("Result is: \n" + str(Solution().swapPairs(Value2Instance(l1))))
+    l1 = list(map(str, list(str(sys.stdin.readline()).strip())))
+    sys.stdout.flush()
+    sys.stdout.write("Input 'K': \n")
+    k = int(sys.stdin.read())
+    sys.stdout.write("Result is: \n" + str(Solution().reverseKGroup(Value2Instance(l1), k)))
